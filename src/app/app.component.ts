621aec676cf30610ead19c61a0508c63d59b12c4
nexus122 <juan-pablo122@hotmail.es>
2023-02-05 15:57:22 +0100

    Quitar console.log y oninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent implements OnInit {
   constructor(private imageService: UnsplashImageService) {}
   async ngOnInit(): Promise<void> {
     this.imageService.getData().subscribe((data) => {
-      console.log(data);
       this.data = data;
       this.columns = this.splitArray(data, this.columnNumber);
     });
@@ -24,7 +23,6 @@ export class AppComponent implements OnInit {
 
   async getMoreData(): Promise<void> {
     this.imageService.getMoreData().subscribe((data) => {
-      console.log(data);
       let temp = [...data, ...this.data];
       this.data = temp;
       this.columns = this.splitArray(temp, this.columnNumber);
@@ -32,33 +30,23 @@ export class AppComponent implements OnInit {
   }
 
   splitArray(originalArray: Array<any>, parts: number) {
-    const arrays = [];
     const chunkLength = Math.floor(originalArray.length / parts);
+    const arrays = [];
 
-    for (let i = 0; i < parts; i++) {
-      if (i === parts - 1) {
-        arrays.push(originalArray.slice(i * chunkLength));
-      } else {
-        arrays.push(
-          originalArray.slice(i * chunkLength, (i + 1) * chunkLength)
-        );
-      }
+    for (let i = 0, start = 0; i < parts; i++, start += chunkLength) {
+      const end = i === parts - 1 ? originalArray.length : start + chunkLength;
+      arrays.push(originalArray.slice(start, end));
     }
 
     return arrays;
   }
 
   windowSize() {
-    console.log('Se recalcula el numero de columnas');
-    console.log('Tamaño: ', window.innerWidth);
     if (window.innerWidth <= 425) {
-      console.log('Es tamaño movil, 1 columna');
       this.columnNumber = 1;
     } else if (window.innerWidth <= 768) {
-      console.log('Es tamaño tablet, 2 columnas');
       this.columnNumber = 2;
     } else if (window.innerWidth >= 1024) {
-      console.log('Es tamaño PC, 3 columnas');
       this.columnNumber = 3;
     }
 
--- src/app/components/columns/columns.component.ts
+++ src/app/components/columns/columns.component.ts
@@ -1,13 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-columns',
   templateUrl: './columns.component.html',
   styleUrls: ['./columns.component.css'],
 })
-export class ColumnsComponent implements OnInit {
+export class ColumnsComponent {
   @Input() columns!: Array<any>;
-  ngOnInit() {
-    console.log('Columnas: ', this.columns);
-  }
 }
